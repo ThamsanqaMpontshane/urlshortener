1855e30c8e91f699e26bab238799023458bc1f41
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2023-07-19 10:19:16 +0200

    👍Done

--- index.js
+++ index.js
@@ -2,22 +2,54 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/index.html');
 });
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+// My code
+let urlDatabase = [];
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  let urlRegex = /^https?:\/\/www\./;
+  if (!urlRegex.test(url)) {
+    res.json({error: 'invalid url'});
+  } else {
+    let shortUrl = Math.floor(Math.random() * 100).toString();
+    let urlObj = {
+      original_url: url,
+      short_url: shortUrl
+    };
+    urlDatabase.push(urlObj);
+    res.json(urlObj);
+  }
+})
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  let shortUrl = req.params.short_url;
+  let urlObj = urlDatabase.find((obj) => {
+    return obj.short_url === shortUrl;
+  }
+  );
+  if (urlObj) {
+    res.redirect(urlObj.original_url)
+  } else {
+    res.json({error: 'No short url found for given input'});
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
